50f5d4fa9ba5b2f12d8445aa5c06940a7ead7d3a
okabe <ashutoshu2411@gmail.com>
2018-02-02 00:44:51 +0530

    server done.

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # networkingLab
 Labs as part of Computer Networking course taught at IIT PKD. 
+
+# LAB 1: Echo server
+
+## Graded (Jan 19, 2018)
+
+1. An echo server that handles multiple clients concurrently.
+
+You can use any programming language that you like, however it would
+be easier if your language has good support for concurrency
+(particularly [green threads]). Some languages known to have good
+concurrency support are Haskell, Erlang, Go, Java and other langauges
+on JVM like scala, clojure. You might also want to stick to the
+language you choose for the rest of the assignments and projects.
+
+
+## Practice problem (to be done in the lab)
+
+1. Write a TCP version of the `netcat` program in your favourite language.
+
+2. Write a UDP version of `netcat` which sends each line in a separate
+   UDP packet. Assume that the maximum length of each line is 1024
+   bytes.
+
+3. 	- Write a simple client that can make dns queries to any dns sever.
+   and resolve the hostname to its ip address.
+
+	- Write a simple dns server does the following: For every query to
+   resolve the hostname `www.james.bond` returns the ip address
+   `007.007.007.007`. Your name server should work with any program
+   like `nslookup` not just your client in step 1.
---
+++ lab2/server.py
@@ -0,0 +1,63 @@
+from socket import *
+import sys
+
+def main():
+	# other clients assume that DNS server is bound to port 53.
+	serverName 		= '127.0.0.1'
+	serverPort 		= 53
+	serverSocket 	= socket(AF_INET, SOCK_DGRAM)
+	serverSocket.bind((serverName, serverPort))
+	while 1:
+		data, addr 	= serverSocket.recvfrom(512)
+		response 	= generateResponse(data)
+		y 			= serverSocket.sendto(response, addr)
+
+# creating dns response
+def generateResponse(data):
+	HEADER 		 = getHeader(data[:12])
+	DOMAIN, typ, clas, dname_b = getQuestion(data[12:])
+	TYPE 		 = (1).to_bytes(2, byteorder = 'big')
+	CLASS 		 = (1).to_bytes(2, byteorder = 'big')
+	TTL 		 = (400).to_bytes(4, byteorder = 'big')
+	RDLENGTH 	 = (4).to_bytes(2, byteorder = 'big')
+	if DOMAIN == ['www','james','bond']:
+		RDATA 		 = (12).to_bytes(1, byteorder = 'big') + (12).to_bytes(1, byteorder = 'big') + (12).to_bytes(1, byteorder = 'big') + (12).to_bytes(1, byteorder = 'big')
+	else :
+		RDATA 		 = (0).to_bytes(1, byteorder = 'big') + (0).to_bytes(1, byteorder = 'big') + (0).to_bytes(1, byteorder = 'big') + (0).to_bytes(1, byteorder = 'big')
+	return HEADER + dname_b + TYPE + CLASS + b'\xc0\x0c' + TYPE + CLASS + TTL + RDLENGTH + RDATA
+
+# creating header for the packet
+def getHeader(data):
+	ID 		 = data[:2]
+	FLAG 	 = int('1' + '0000' + '0' + '0' + '0' + '0' + '000' + '0000', 2).to_bytes(2, byteorder = 'big')
+	# 		 (QR + OPCODE + AA + TC + RD + RA + Z  + RCODE)
+	QDCOUNT	 = (1).to_bytes(2, byteorder = 'big')
+	ANCOUNT	 = (1).to_bytes(2, byteorder = 'big')
+	NSCOUNT	 = (0).to_bytes(2, byteorder = 'big')
+	ARCOUNT	 = (0).to_bytes(2, byteorder = 'big')
+	return ID + FLAG + QDCOUNT + ANCOUNT + NSCOUNT + ARCOUNT
+
+# extracting the domain form the query
+def getQuestion(data):
+	DOMAIN 	= []
+	tmp 	= ''
+	length 	= 0
+	index 	= 0
+	for i in data:
+		index = index + 1
+		if(length != 0):
+			tmp 	= tmp + chr(i)
+			length 	= length - 1
+		else :
+			DOMAIN.append(tmp)
+			tmp 	= ''
+			length 	= int(i)
+			if(int(i) == 0):
+				break
+	DOMAIN 	= DOMAIN[1:]
+	TYPE 	= data[index:index + 2]
+	index 	= index + 2
+	CLASS 	= data[index:index + 2]
+	return (DOMAIN, TYPE, CLASS, data[:index - 2])
+
+main()
